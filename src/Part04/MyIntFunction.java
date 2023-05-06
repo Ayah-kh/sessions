c1fca71e267247af3b54d9733e70c30de35db710
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-05-06 17:57:01 +0300

    First Commit

--- src/Part04/MyIntFunction.java
+++ src/Part04/MyIntFunction.java
@@ -1,20 +1,19 @@
 package Part04;
 
-import java.util.function.IntFunction;
-
 @FunctionalInterface
 public interface MyIntFunction {
     Integer apply(Integer a);
 
-    default MyIntFunction compose(MyIntFunction before){
-        return a->apply(before.apply(a));
+    default MyIntFunction compose(MyIntFunction before) {
+        return a -> apply(before.apply(a));
     }
-    default MyIntFunction compose2(MyIntFunction before){
-        return a-> {
+
+    default MyIntFunction compose2(MyIntFunction before) {
+        return a -> {
             Integer applyFirst = before.apply(a);
             return apply(applyFirst);
         };
-        }
     }
+}
 
 
