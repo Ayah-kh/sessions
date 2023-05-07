20d858199f2a9ecca0d2338fcf858eb467c38389
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-05-07 10:08:02 +0300

    First Commit

--- src/Part05/Application.java
+++ src/Part05/Application.java
@@ -22,12 +22,21 @@ public class Application {
         System.out.println("add1.apply(4) = " + add1.apply(4));
         MyCurBiFunction<Integer, Integer, Integer> add3 = convert(add);
         System.out.println("add3.apply(4).apply(5) = " + add3.apply(4).apply(5));
+        System.out.println("--------------------");
+        MyCurBiFunction<Integer,Integer,Integer> MultCurr=a->b->a*b;
+        System.out.println("MultCurr.apply(3).apply(4) = " + MultCurr.apply(3).apply(4));
+        MyBiFunction<Integer, Integer, Integer> MultiBi = convert(MultCurr);
+        System.out.println("MultiBi.apply(3,4) = " + MultiBi.apply(3, 4));
+
 
     }
 
 
     static <T,U,V> MyCurBiFunction<T,U,V> convert (MyBiFunction<T,U,V> biFunction){
         return a->b->biFunction.apply(a,b);
+    }
+    static <T,U,V> MyBiFunction<T,U,V> convert (MyCurBiFunction<T,U,V> biCurFunction){
+        return (a,b)->biCurFunction.apply(a).apply(b);
     }
 
     static  class Add implements MyCurBiFunction<Integer,Integer,Integer>{
