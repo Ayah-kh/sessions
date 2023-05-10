5a17618ab78f5324b5ded729993c12bea92257df
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-05-10 20:51:38 +0300

    First Commit

--- src/Part06/Application.java
+++ src/Part06/Application.java
@@ -1,5 +1,20 @@
 package Part06;
 
 public class Application {
+    public static void main(String[] args) {
+        MyFunction<String, String> helloMsg = a -> "Hello " + a;
+        String ahamd = helloMsg.apply("ahamd");
+        System.out.println("ahamd = " + ahamd);
+
+        MyFunction<Integer, Integer> inc = a -> a + 1;
+        Integer num = inc.apply(5);
+        System.out.println("num = " + num);
+
+        MyFunction<String,Integer> strLength=a->a.length();
+        System.out.println("strLength.apply(\"Mohammed\") = " + strLength.apply("Mohammed"));
+
+
+    }
+
 
 }
