c6844f83bfca909a2e31d5e79d73fae6ae6e0872
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-05-12 20:06:36 +0300

    First Commit

--- src/Part08/Application.java
+++ src/Part08/Application.java
@@ -1,6 +1,9 @@
 package Part08;
 
-import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class Application {
@@ -12,6 +15,7 @@ public class Application {
             Student.of("DANA", 40, Gender.FEMALE, true, Course.MATH, Course.ENGLISH, Course.PHYSICS),
             Student.of("RUBA", 22, Gender.FEMALE, true, Course.MATH, Course.PHYSICS)
     };
+
     public static void main(String[] args) {
 
         Stream.of(students)
@@ -29,7 +33,46 @@ public class Application {
 
         System.out.println("=============================================");
 
+        Stream<Course> courseStream = Stream.of(students)
+                .flatMap(student -> student.getCourse().stream());
+
+        Stream<Stream<Course>> streamStream = Stream.of(students)
+                .map(student -> student.getCourse().stream());
+
+        List<String> studentsName = Stream.of(students)
+                .map(Student::getName)
+                .collect(Collectors.toList());
+        System.out.println("studentsName = " + studentsName);
+        System.out.println("=============================================");
+
+        Map<Gender, List<Student>> collect = Stream.of(students)
+                .collect(Collectors.groupingBy(Student::getGender));
+
+        System.out.println("collect = " + collect);
+        System.out.println("=============================================");
+
+        Map<Gender, List<String>> collect1 = Stream.of(students)
+                .collect(Collectors.groupingBy(Student::getGender,
+                        Collectors.mapping(Student::getName, Collectors.toList())));
+
+        System.out.println("collect1 = " + collect1);
+
+        System.out.println("=============================================");
+
+        Stream.of(students)
+                .map(Student::getName)
+                .sorted()
+                .forEach(System.out::println);
+
+        Stream<String> stringStream = Stream.of(students)
+                .map(Student::getName);
+
+        int sum = IntStream.rangeClosed(1, 10)
+                .reduce(0, (acc, e) -> acc + e);
+        System.out.println("=============================================");
+        System.out.println("sum = " + sum);
 
 
-}
+
+    }
 }
