b670303694cd53b93f94c496f5366804f6040108
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-05-14 00:05:48 +0300

    First Commit

--- src/Part09/Application.java
+++ src/Part09/Application.java
@@ -5,7 +5,7 @@ import java.util.function.Function;
 public class Application {
     public static void main(String[] args) {
         Integer sum = reduceL(0, acc -> e -> acc + e, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-        Integer max = reduceL(0, acc -> e -> acc > e ? acc : e, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        Integer max = max(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
         System.out.println("sum = " + sum);
         System.out.println("max = " + max);
     }
@@ -22,6 +22,17 @@ public class Application {
             accRes = acc.apply(accRes).apply(data[i]);
         }
 
+        return accRes;
+    }
+
+    public static <U, T> U reduceR
+            (U seed, Function<U, Function<T, U>> accFunction, T... data) {
+        if (data == null || data.length == 0)
+            return seed;
+        U accRes = seed;
+        for (int i = data.length; i > 0; i--) {
+            accRes = accFunction.apply(accRes).apply(data[i]);
+        }
         return accRes;
     }
 
