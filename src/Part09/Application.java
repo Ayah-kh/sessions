930ecd571291ed377bbe2a074b74a6b8bae7acea
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-05-14 00:16:23 +0300

    First Commit

--- src/Part09/Application.java
+++ src/Part09/Application.java
@@ -1,5 +1,6 @@
 package Part09;
 
+import java.util.ArrayList;
 import java.util.function.Function;
 
 public class Application {
@@ -8,18 +9,34 @@ public class Application {
         Integer max = max(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
         System.out.println("sum = " + sum);
         System.out.println("max = " + max);
+
+        Integer sum2 = reduceR(0, acc -> e -> acc + e,
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        System.out.println("sum2 = " + sum2);
+
+        ArrayList<Integer> integers = toList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        System.out.println("integers = " + integers);
+    }
+
+    public static <T> ArrayList<T> toList(T...data){
+        return reduceL(new ArrayList<T>(),
+                acc->e-> {
+                    acc.add(e);
+                    return acc;}
+                ,data);
+
     }
 
 
     public static <U, T> U
-    reduceL(U seed, Function<U, Function<T, U>> acc, T... data) {
+    reduceL(U seed, Function<U, Function<T, U>> accFunction, T... data) {
 
         if (data == null || data.length == 0)
             return seed;
         U accRes = seed;
 
         for (int i = 0; i < data.length; i++) {
-            accRes = acc.apply(accRes).apply(data[i]);
+            accRes = accFunction.apply(accRes).apply(data[i]);
         }
 
         return accRes;
@@ -30,7 +47,7 @@ public class Application {
         if (data == null || data.length == 0)
             return seed;
         U accRes = seed;
-        for (int i = data.length; i > 0; i--) {
+        for (int i = data.length-1; i >= 0; i--) {
             accRes = accFunction.apply(accRes).apply(data[i]);
         }
         return accRes;
