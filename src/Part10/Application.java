011b78a9576856ba2c9c9440917bbf657e6f56dd
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-05-14 10:01:12 +0300

    First Commit

---
+++ src/Part10/Application.java
@@ -0,0 +1,8 @@
+package Part10;
+
+public class Application {
+    public static void main(String[] args) {
+        ExpandedArray<Integer> numbers=
+                ExpandedArray.of(1,2,3,4,5,6,7,8,9,10);
+    }
+}
