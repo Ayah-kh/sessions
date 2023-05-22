d1d51cb64709b6e0aa6deec11d012cff81eb151f
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-05-22 16:07:45 +0300

    First Commit

--- src/Part12/SimpleCounter.java
+++ src/Part12/SimpleCounter.java
@@ -1,5 +1,7 @@
 package Part12;
 
+import java.util.concurrent.CountDownLatch;
+
 public class SimpleCounter {
     private int counter;
 
@@ -13,12 +15,20 @@ public class SimpleCounter {
 
     public static void main(String[] args) throws InterruptedException {
         SimpleCounter simpleCounter=new SimpleCounter();
-        int numberOfThreads=10;
+        int numberOfThreads=1000;
+        int numberOfIteration=1000;
+        CountDownLatch downLatch = new CountDownLatch(numberOfThreads);
 
         for (int i = 0; i <numberOfThreads ; i++) {
-            new Thread(()->simpleCounter.inc()).start();
+            new Thread(()-> {
+                for (int i1 = 0; i1 < numberOfIteration; i1++) {
+
+                    simpleCounter.inc();
+                }
+                downLatch.countDown();
+            }).start();
         }
-        Thread.sleep(2000);
+        downLatch.await();
         System.out.println("simpleCounter.counter = " + simpleCounter.counter);
     }
 }
