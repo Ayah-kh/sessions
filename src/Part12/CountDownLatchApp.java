62295b47a16470245d650a53cd4272d7f893d6ef
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-05-23 16:12:55 +0300

    First Commit

--- src/Part12/CountDownLatchApp.java
+++ src/Part12/CountDownLatchApp.java
@@ -1,4 +1,42 @@
 package Part12;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class CountDownLatchApp {
+    static AtomicInteger atomicInteger = new AtomicInteger(0);
+
+    public static void main(String[] args) throws InterruptedException {
+        CountDownLatch startLatch = new CountDownLatch(1);
+
+        int numberOfThreads = 3;
+        CountDownLatch endLatch = new CountDownLatch(numberOfThreads);
+        for (int i = 0; i < numberOfThreads; i++) {
+            new Thread(() -> {
+                System.out.println("Thread started");
+
+                try {
+                    startLatch.await();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                System.out.println("Now do Thread operation");
+                atomicInteger.incrementAndGet();
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                endLatch.countDown();
+            }).start();
+        }
+        System.out.println("Wait 4 seconds");
+        Thread.sleep(6000);
+        startLatch.countDown();
+
+        System.out.println("Wait all thread to complete ");
+
+        endLatch.await();
+
+    }
 }
---
+++ src/Part12/CounterUsingStream.java
@@ -0,0 +1,27 @@
+package Part12;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.IntStream;
+
+public class CounterUsingStream {
+    private AtomicInteger counter=new AtomicInteger(0);
+
+    private void inc(){
+        counter.incrementAndGet();
+    }
+    public static void main(String[] args) {
+
+        CounterUsingStream counterApp = new CounterUsingStream();
+        int numberOfThreads = 1000;
+        int numberOfIteration = 1000;
+        CountDownLatch downLatch = new CountDownLatch(numberOfThreads);
+
+        IntStream.rangeClosed(1,numberOfThreads)
+                .mapToObj(i)
+
+
+
+
+    }
+}
