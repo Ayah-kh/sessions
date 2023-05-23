126cb6364b5a6f1d18adec27e672a55b9a64efcb
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-05-23 15:49:52 +0300

    First Commit

--- src/Part12/JoinApplication.java
+++ src/Part12/JoinApplication.java
@@ -1,4 +1,34 @@
 package Part12;
 
+import java.util.concurrent.CountDownLatch;
+
 public class JoinApplication {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        CountDownLatch downLatch = new CountDownLatch(1);
+        Thread thread = new Thread(() -> {
+            try {
+                Thread.sleep(6000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+
+            System.out.println("Done");
+            downLatch.countDown();
+        });
+
+        thread.start();
+
+        System.out.println("Main Thread");
+
+        System.out.println("Do another operation");
+
+//        thread.join();
+        downLatch.await();
+        System.out.println("Thread joined main thread");
+
+        //
+
+    }
 }
