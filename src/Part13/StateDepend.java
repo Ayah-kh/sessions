873cedfdd0118d01bdbbacd0ebacd4c43d360902
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-05-28 19:53:47 +0300

    First Commit

--- src/Part13/StateDepend.java
+++ src/Part13/StateDepend.java
@@ -3,6 +3,7 @@ package Part13;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 public class StateDepend {
 
@@ -11,11 +12,15 @@ public class StateDepend {
 
         List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
 
-        Iterator<Integer> iterator = numbers.iterator();
+        Iterator<Integer> numIterator = numbers.iterator();
 
-        while (true){
-            Integer num = iterator.next();
-            System.out.println("num = " + num);
+        while (numIterator.hasNext()){
+            try {
+                Integer num = numIterator.next();
+                System.out.println("num = " + num);
+            }catch (NoSuchElementException e){
+                break;
+            }
         }
     }
 
