270caaf8d98e8363624e3a30fd33289a9a81f82c
Ayah.kh GitHub <ayah.java.dev@gmail.com>
2023-05-28 19:38:20 +0300

    First Commit

--- src/Part13/Main.java
+++ src/Part13/Main.java
@@ -2,19 +2,9 @@ package Part13;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            int a = 3/0;
-        }catch (ArithmeticException e) {
-            System.out.println("div by 0");
-        }
 
-        System.out.println("Hello");
-
-        try {
-            sleepV1(2);
-        } catch (InterruptedException e) {
-            System.out.println("Sleep interrupt");
-        }
+        int[] numbers={1,2,3,4,5};
+        System.out.println("numbers[5] = " + numbers[5]);
 
 
     }
